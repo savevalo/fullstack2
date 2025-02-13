6e4fc2b02ee475d9480bc70b31bdee4b611d32d4
santtu <santtuvalosaari@gmail.com>
2025-02-13 15:27:42 +0200

    added mongo.js

---
+++ part3/phonebookFront/mongo.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose"
+
+
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://savevalo:${password}@cluster0.zxld0.mongodb.net/personsApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length<4) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+
+
+const person = new Person({
+  name: name,
+  number: number,
+})
+
+person.save().then(result => {
+  console.log(`added ${name} number ${number} to phonebook`)
+  mongoose.connection.close()
+})
+
+
+Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+})
+}
